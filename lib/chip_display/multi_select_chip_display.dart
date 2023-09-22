7c0e77754078ee6efc48f7caed14fb2b2035bef6
Julien Merly <julien.merly@edigitalsolution.onmicrosoft.com>
2023-09-22 19:07:28 +0200

    checkmark

--- lib/chip_display/multi_select_chip_display.dart
+++ lib/chip_display/multi_select_chip_display.dart
@@ -130,7 +130,7 @@ class MultiSelectChipDisplay<V> extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(2.0),
       child: ChoiceChip(
-        iconTheme: IconThemeData(color: Colors.transparent),
+        showCheckmark: false,
         shape: shape as OutlinedBorder?,
         avatar: icon != null
             ? Icon(
