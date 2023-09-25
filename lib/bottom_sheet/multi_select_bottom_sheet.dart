1976babdc0a8a378848d03d0c2623a62f1d7e551
Julien Merly <julien.merly@edigitalsolution.onmicrosoft.com>
2023-09-25 17:37:38 +0200

    test

--- lib/bottom_sheet/multi_select_bottom_sheet.dart
+++ lib/bottom_sheet/multi_select_bottom_sheet.dart
@@ -334,23 +334,21 @@ class _MultiSelectBottomSheetState<T> extends State<MultiSelectBottomSheet<T>> {
                 thickness: 0.3,
                 color: Theme.of(context).canvasColor,
               ),
-              Expanded(
-                child: widget.listType == null ||
-                        widget.listType == MultiSelectListType.LIST
-                    ? ListView.builder(
-                        controller: scrollController,
-                        itemCount: _items.length,
-                        itemBuilder: (context, index) {
-                          return _buildListItem(_items[index]);
-                        },
-                      )
-                    : Container(
-                        padding: EdgeInsets.all(10),
-                        child: Wrap(
-                          children: _items.map(_buildChipItem).toList(),
-                        ),
+              widget.listType == null ||
+                      widget.listType == MultiSelectListType.LIST
+                  ? ListView.builder(
+                      controller: scrollController,
+                      itemCount: _items.length,
+                      itemBuilder: (context, index) {
+                        return _buildListItem(_items[index]);
+                      },
+                    )
+                  : Container(
+                      padding: EdgeInsets.all(10),
+                      child: Wrap(
+                        children: _items.map(_buildChipItem).toList(),
                       ),
-              ),
+                    ),
               Padding(
                   padding: const EdgeInsets.symmetric(vertical: 16),
                   child: Material(
